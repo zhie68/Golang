ead3337242c0a14ec00083493a40012afdb2da2c
Muhammad Fauji <88869085+zhie68@users.noreply.github.com>
2023-11-03 10:37:20 +0700

    Update DiagonalDiv.go

--- DiagonalDiv.go
+++ DiagonalDiv.go
@@ -12,7 +12,18 @@ import (
 
 /*
  * Complete the 'diagonalDifference' function below.
- *
+ example input  
+ 3
+ 11 2 4
+ 4 5 6
+ 10 8 -12
+
+ example output
+ 11 + 5 - 12 = 4
+ 4 + 5 + 10 = 19
+ 
+ absolute(Difference: |4 - 19| = 15)
+
  * The function is expected to return an INTEGER.
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
